fc6f248b0f2b7bc3ee804c712760fabf650ad07f
Youssef Sadek <youssef.sadek@gmail.com>
2021-09-07 10:08:10 +0200

    refactored

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,29 +13,26 @@ const GYRO_CHARACTERISTIC = 'ff278bc5-3e44-4d81-9817-4c20a1efea70';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  private static readonly FOLDER_PATH = 'imu';
+  private static readonly FILE_PATH_SUFFIX = '-data.csv';
   private device: BleDevice;
+
   public connected = new BehaviorSubject<boolean>(false);
   public showConnect = false;
-  public xAcceleration: string;
-  public yAcceleration: string;
-  public zAcceleration: string;
-  public xGyro: string;
-  public yGyro: string;
-  public zGyro: string;
+
+  public xAcceleration: string = '0.00';
+  public yAcceleration: string = '0.00';
+  public zAcceleration: string = '0.00';
+  public xGyro: string = '0.00';
+  public yGyro: string = '0.00';
+  public zGyro: string = '0.00';
 
   constructor(private ngZone: NgZone) {
-    this.xAcceleration = '0.00';
-    this.yAcceleration = '0.00';
-    this.zAcceleration = '0.00';
-    this.xGyro = '0.00';
-    this.yGyro = '0.00';
-    this.zGyro = '0.00';
-
     this.connected.subscribe(() => {
-      from(Filesystem.mkdir({
-        path: 'imu',
+      Filesystem.mkdir({
+        path: Tab1Page.FOLDER_PATH,
         directory: Directory.Documents,
-      })).subscribe(() => this.update())
+      }).finally(() => this.update())
     });
   }
 
@@ -53,7 +50,7 @@ export class Tab1Page {
         this.device = d;
         from(BleClient.connect(this.device.deviceId)).subscribe(() => {
           this.connected.next(true);
-          console.log('connected to device', this.device);
+          console.log('connected to device', this.device.deviceId);
         });
       });
     })
@@ -73,7 +70,7 @@ export class Tab1Page {
           this.ngZone.run(() => {
             [this.xAcceleration, this.yAcceleration, this.zAcceleration] = this.bytesToString(value.buffer.slice(value.byteOffset)).split(',');
             Filesystem.appendFile({
-              path: `imu/${new Date().getTime()}-acc-data.csv`,
+              path: `${Tab1Page.FOLDER_PATH}/${this.getFilePrefix()}acc${Tab1Page.FILE_PATH_SUFFIX}`,
               data: [new Date().getTime(), this.xAcceleration, this.yAcceleration, this.zAcceleration].join(',') + '\n',
               directory: Directory.Documents,
               encoding: Encoding.UTF8,
@@ -89,7 +86,7 @@ export class Tab1Page {
           this.ngZone.run(() => {
             [this.xGyro, this.yGyro, this.zGyro] = this.bytesToString(value.buffer.slice(value.byteOffset)).split(',');
             Filesystem.appendFile({
-              path: `imu/${new Date().getTime()}-gyro-data.csv`,
+              path: `${Tab1Page.FOLDER_PATH}/${this.getFilePrefix()}gyro${Tab1Page.FILE_PATH_SUFFIX}`,
               data: [new Date().getTime(), this.xGyro, this.yGyro, this.zGyro].join(',') + '\n',
               directory: Directory.Documents,
               encoding: Encoding.UTF8,
@@ -98,6 +95,11 @@ export class Tab1Page {
         }
       )
     }
+  }
+
+  private getFilePrefix(): string {
+    const date = new Date();
+    return `${date.getFullYear()}-${date.getMonth()}-${date.getDay() + 1}-`
   }
 
 }
